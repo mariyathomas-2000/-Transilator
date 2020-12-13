207da3b3b25dadd105e924400c40b36fd4a57002
Mariya Thomas <75537255+mariyathomas-2000@users.noreply.github.com>
2020-12-13 13:08:43 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import ttk
+from methods import Mytranslator
+app=Tk()
+app.geometry('350x520')
+app.title('Google Translate')
+app.resizable(0,0)
+app.config(bg='blue')
+def get():
+    s=srcLangs.get()
+    message=sourceText.get(1.0,END)
+    translator=Mytranslator()
+    text=translator.run(txt=message,src=s,dest=destLangs.get())
+    destText.delete(1.0,END)
+    destText.insert(END,text)
+
+appName=Label(app,text='Google Translate',font=('arial',20),
+              bg='green',fg='black',height=2)
+appName.pack(side=TOP,fill=BOTH,pady=0)
+version=Label(app,text='beta',bg='green').place(x=250,y=45)
+frame=Frame(app).pack(side=BOTTOM)
+sourceText=Text(frame,font=('arial',10),height=11,wrap=WORD)
+sourceText.pack(side=TOP,padx=5,pady=5)
+
+transBtn=Button(frame,text='Translate',font=('arial',10,'bold'),
+                fg='red',bg='blue',activebackground='green',relief=GROOVE,command=get)
+transBtn.pack(side=TOP,pady=15)
+langs=Mytranslator().langs
+
+srcLangs=ttk.Combobox(frame,values=langs,width=10)
+srcLangs.place(x=30,y=280)
+srcLangs.set('english')
+
+destLangs=ttk.Combobox(frame,values=langs,width=10)
+destLangs.place(x=240,y=280)
+destLangs.set('hindi')
+
+destText=Text(frame,font=('arial',10),height=11,wrap=WORD)
+destText.pack(side=TOP,padx=5,pady=5)
+app.mainloop()#
